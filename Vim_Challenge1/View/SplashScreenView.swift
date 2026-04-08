ccee6f139b52f5a9636342278ea1fc8bb528f6a4
josandapw <josandapw@gmail.com>
2026-04-08 11:12:56 +0700

    feat: finishing splash screen

--- Vim_Challenge1/View/SplashScreenView.swift
+++ Vim_Challenge1/View/SplashScreenView.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct SplashScreenView: View {
     var body: some View {
         VStack {
-            ///Ini nanti diganti icon
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.darkBlue)
+            Image("vimIcon")
+                .resizable()
                 .frame(width: 150, height: 150)
-                .padding(10)
+            ///Ini nanti diganti icon
+            // RoundedRectangle(cornerRadius: 25, style: .continuous)
+                // .fill(.darkBlue)
+                // .frame(width: 150, height: 150)
+                // .padding(10)
             Text("Welcome to")
                 .font(.system(size: 34, weight: .bold))
                 .multilineTextAlignment(.center)
@@ -29,41 +32,62 @@ struct SplashScreenView: View {
             VStack(alignment: .leading) {
                 ///ganti icon
                 HStack {
-                    Circle()
-                        .fill(.darkBlue)
-                        .frame(width: 60, height: 60)
+                    // Circle()
+                        // .fill(.darkBlue)
+                        // .frame(width: 60, height: 60)
+                    Image(systemName: "target")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .scaledToFit()
+                        .foregroundColor(.darkBlue)
                     VStack(alignment: .leading, spacing: 2) {
                         Text("Monster Battle")
                             .font(.system(size: 12, weight: .bold))
                             .multilineTextAlignment(.leading)
+                            .padding(.horizontal, 9)
                         Text("Set your own challenge! Choose your calorie target to define the monster's health bar.")
                             .font(.system(size: 10, weight: .regular))
+                            .padding(.horizontal, 10)
                     }
                 }
                 ///ganti icon
                 HStack {
-                    Circle()
-                        .fill(.darkBlue)
-                        .frame(width: 60, height: 60)
+                    // Circle()
+                        // .fill(.darkBlue)
+                    // .frame(width: 60, height: 60)
+                    Image(systemName: "figure.run")
+                        .resizable()
+                        .frame(width: 40, height: 45)
+                        .scaledToFit()
+                        .foregroundColor(.darkBlue)
                     VStack(alignment: .leading, spacing: 2) {
                         Text("Track Your Progress")
                             .font(.system(size: 12, weight: .bold))
                             .multilineTextAlignment(.leading)
+                            .padding(.horizontal, 9)
                         Text("View your battle strikes in Journey. Collect badges as proof of your undefeated streak!")
                             .font(.system(size: 10, weight: .regular))
+                            .padding(.horizontal, 10)
                     }
                 }
                 ///ganti icon
                 HStack {
-                    Circle()
-                        .fill(.darkBlue)
-                        .frame(width: 60, height: 60)
+                    // Circle()
+                        // .fill(.darkBlue)
+                        // .frame(width: 60, height: 60)
+                    Image(systemName: "trophy.fill")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .scaledToFit()
+                        .foregroundColor(.darkBlue)
                     VStack(alignment: .leading, spacing: 2) {
                         Text("Attack With Exercise")
                             .font(.system(size: 12, weight: .bold))
                             .multilineTextAlignment(.leading)
+                            .padding(.horizontal, 9)
                         Text("Set your own challenge! Choose your calorie target to define the monster's health bar.")
                             .font(.system(size: 10, weight: .regular))
+                            .padding(.horizontal, 10)
                     }
                     
             }
