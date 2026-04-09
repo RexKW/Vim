58965ebfb7aa765862ab21f607c2caef4bb3d2de
Rex Kenny Wirasantoso <153161558+RexKW@users.noreply.github.com>
2026-04-09 08:48:24 +0700

    feat: +Added workout utilities for the workout session, +implemented healthkit capabilities including info.plist

---
+++ Vim_Challenge1/Utilities/WorkoutState.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutState.swift
+//  HealthTrackTest
+//
+//  Created by Rex Kenny Wirasantoso on 07/04/26.
+//
+
+import Foundation
+
+enum WorkoutState {
+    case idle
+    case preparing
+    case active
+    case paused
+    case finished
+}
---
+++ Vim_Challenge1/Viewmodel/WorkoutViewModel+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutViewModel+Extension.swift
+//  Vim_Challenge1
+//
+//  Created by Rex Kenny Wirasantoso on 09/04/26.
+//
+
+import Foundation
