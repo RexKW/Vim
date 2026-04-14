c944f59860956194c452024e68604ea67b7d8e91
Sharon Tan <shasatanjaya@gmail.com>
2026-04-14 10:34:19 +0700

    feat: design ui congrats

--- Vim_Challenge1/View/CongratsView.swift
+++ Vim_Challenge1/View/CongratsView.swift
@@ -14,28 +14,41 @@ struct CongratsView: View {
     var body: some View {
         NavigationStack {
             VStack{
-                Text("Congratulations! 🎉")
-                    .font(.system(size: 34, weight: .bold))
-                    .multilineTextAlignment(.center)
-                Text("You defeated the monster! What's next for your goal?")
-                    .padding(.top,-1)
-                    .multilineTextAlignment(.center)
-                Image(.deadRex)
-                    .resizable()
-                    .frame(width: 180, height: 200)
-                    .padding(.top, 20)
-                
-                Button (action: {
-                    isPresented = true
-                }){
-                    Text("Set Calories")
-                        .fontWeight(.semibold)
-                        .padding(.vertical, 12)
-                        .padding(.horizontal, 110)
+                Spacer()
+                VStack(alignment: .center, spacing: 10){
+                    Text("Sheesh! Victory 🎉")
+                        .font(.system(size: 34, weight: .bold))
+                        .multilineTextAlignment(.center)
+                    Text("You absolutely crushed that monster and finished your exercise. That’s how a champ does it!")
+                        .multilineTextAlignment(.center)
+                    
+                    Image(.deadRex)
+                        .resizable()
+                        .frame(width: 180, height: 200)
+                        .padding(.top, 20)
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.darkBlue)
-                .padding(.top, 60)
+                .padding(.horizontal, 32)
+                
+                Spacer()
+                VStack{
+                    Text("Ready to keep this \(Text("momentum").foregroundColor(.vibrantOrange).bold()) going for your next goal?")                .multilineTextAlignment(.center)
+                        .fontWeight(.semibold)
+                        .padding(.top, 50)
+                        .padding(.horizontal, 32)
+                    
+                    Button (action: {
+                        isPresented = true
+                    }){
+                        Text("Set Calories")
+                            .fontWeight(.semibold)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 110)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.darkBlue)
+                    .padding(.top, 10)
+                }
+                .padding(.bottom, 10)
             }
             .navigationDestination(isPresented: $isPresented) {
                 CalorieTargetView()
