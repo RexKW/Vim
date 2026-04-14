08bc1faa1154d1459a6fd37f73f295c846f1c04d
Sharon Tan <shasatanjaya@gmail.com>
2026-04-14 09:37:15 +0700

    fix: journeySheetView kalo dead masih nyala

--- Vim_Challenge1/View/MonsterCardView.swift
+++ Vim_Challenge1/View/MonsterCardView.swift
@@ -22,14 +22,14 @@ struct MonsterCardView: View {
                 .frame(width: 150, height: 120)
                 .offset(y: 30)
                 .clipShape(.rect)
-                .grayscale(monster.status != "In Progress" ? 1.0 : 0.0)
+                .grayscale(monster.status == "Locked" || monster.status == "Coming Soon" ? 1.0 : 0.0)
                 .padding(.top, 10)
             
             Divider()
                 .frame(height: 1)
             
             VStack(spacing: 5){
-                Text(monster.status == "In Progress" ? monster.name : (monster.status == "Coming Soon" ? "Coming Soon.." : "???"))
+                Text(monster.status == "In Progress" || monster.status == "Dead" ? monster.name : (monster.status == "Coming Soon" ? "Coming Soon.." : "???"))
                     .fontWeight(monster.status == "In Progress" ? .bold : .regular)
                     .font(.subheadline)
                     .padding(.top, 10)
@@ -38,9 +38,9 @@ struct MonsterCardView: View {
                     .padding(2)
                     .padding(.horizontal, 4)
                     .font(.caption2)
-                    .foregroundStyle(monster.status == "In Progress" || monster.status == "Completed" ? Color.green : Color.gray)
+                    .foregroundStyle(monster.status == "In Progress" ? Color.green : Color.gray)
                     .background(
-                        (monster.status == "In Progress" || monster.status == "Completed")
+                        (monster.status == "In Progress" || monster.status == "Dead")
                         ? Color.green.opacity(0.1)
                         : (monster.status == "Coming Soon" ? Color.clear : Color.gray.opacity(0.1)))
                     .cornerRadius(4)
@@ -52,7 +52,7 @@ struct MonsterCardView: View {
         }
         .frame(maxWidth: .infinity)
         .padding(5)
-        .background(monster.status == "In Progress" || monster.status == "Completed" ? Color.white : Color.gray.opacity(0.1))
+        .background(monster.status == "In Progress" || monster.status == "Dead" ? Color.white : Color.gray.opacity(0.1))
         .cornerRadius(12)
         .overlay(
             
@@ -73,7 +73,7 @@ struct MonsterCardView: View {
             hp: 0,
             image: "Rex",
             deadImage: "",
-            status: "In Progress"
+            status: "Dead"
         )
     )
 }
